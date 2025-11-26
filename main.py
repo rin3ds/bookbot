87bab503090056473aa6eb4de5957f0ea67e2017
rin3ds <rin.estuita@gmail.com>
2025-11-27 03:25:14 +0800

    C: Arguments correction

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import sys
-
 from stats import get_book_text, get_num_words, count_characters
 
-book_path = sys.argv[1]
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
-    
     num_words = get_num_words(text)
-    
     char_stats = count_characters(text)
 
     print("============ BOOKBOT ============")
